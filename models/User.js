5038f4bef0910c7e7fb1e25fa50b588f7c1fff16
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-14 02:32:09 +0700

    add User models✨

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const monggoose = require('mongoose')
+
+const userSchema = new monggoose.Schema({
+    nama: { type: String, required: true, max: 255 },
+    email: { type: String, required: true, max: 100 },
+    password: { type: String, required: true, min: 6, max: 1024 },
+    createdAt: { type: Date, default: Date.now },
+})
+
+module.exports = monggoose.model('User', userSchema)
